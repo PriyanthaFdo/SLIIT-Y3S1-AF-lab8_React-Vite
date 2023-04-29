3e380d26298118f8fcea7a07267cf8e798a822c9
Priyantha.Kingslake <Priyantha.Kingslake@gmail.com>
2023-04-29 09:56:20 +0530

    exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Greetings from './components/Greetings'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Greetings />
+      <NavBar />
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+  return (
+    <>
+      <ul style={{}}>
+        <a href="#"><li>link 1</li></a>
+        <a href="#"><li>link 2</li></a>
+        <a href="#"><li>link 3</li></a>
+        <a href="#"><li>link 4</li></a>
+        <a href="#"><li>link 5</li></a>
+      </ul>
+    </>
+  );
+}
+export default NavBar;
